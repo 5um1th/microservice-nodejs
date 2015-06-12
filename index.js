6a86d197f9e0bf6c67b576b5b3994d194b9db97e
Alex Stoica <dimitrie.stoica@canopy-cloud.com>
2015-06-12 16:42:31 +0100

    simple node.js app

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+
+// Constants
+var PORT = 8080;
+
+// App
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Hello world\n');
+});
+
+app.get('/alex', function (req,res) {
+  res.send('Hello Alex');
+});
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
