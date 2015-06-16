ab58a9b42b7847ac0a42d7ebc0f6b7de2e2faf4d
Alex Stoica <dimitrie.stoica@canopy-cloud.com>
2015-06-16 18:38:05 +0100

    Added /info route

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ var config  = require('./config');
 var path    = require('path');
 var app     = express();
 var server  = require('http').createServer(app);
+var info    = require('./lib/controllers/info');
 
 app.get('/', function (req, res) {
   res.send('Hello world\n');
+});
+
+app.get('/info', function (req,res) {
+  res.send(info.showInfo());
 });
 
 app.use(function(req, res, next) {
