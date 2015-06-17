4e01471b5bf7f9648878aeaba0b2f06f684c0638
Alex Stoica <dimitrie.stoica@canopy-cloud.com>
2015-06-17 14:12:15 +0100

    RabbitMQ config added

--- config.js
+++ config.js
@@ -8,6 +8,13 @@ config.express = {
   ip: "127.0.0.1"
 };
 
+config.rabbitmq = {
+  port: process.env.RABBITMQ_PORT || 5672,
+  host: process.env.RABBITMQ_HOST || "localhost", 
+  login: process.env.RABBITMQ_USER || "guest",
+  password: process.env.RABBITMQ_PASSWORD || "guest",
+};
+
 if (PRODUCTION) {
   config.express.ip = "0.0.0.0";
   config.express.port = process.env.EXPRESS_PORT || 80;
@@ -17,5 +24,7 @@ if (TEST) {
   config.express.port = process.env.EXPRESS_PORT || 4657;
 }
 
+config.rabbitmqURL = "amqp://" + config.rabbitmq.login + ":" + config.rabbitmq.password 
+  + "@" + config.rabbitmq.host + ":" + config.rabbitmq.port;
+
 config.currentENV = currentENV;
-console.log(config.currentENV)
--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var app    = require('./index');
 var config = require('./config');
 var bole   = require('bole');
+var amqp   = require('amqp');
 
 bole.output({level: "debug", stream: process.stdout});
 var log = bole("server");
 log.info({env: process.env.NODE_ENV}, "Server process starting");
+
+console.log(config.rabbitmqURL)
+var rabbitmq = amqp.createConnection(config.rabitmqURL);
+rabbitmq.on('ready', function (callback) {
+  log.info("connected to: " + config.rabbitmqURL);
+});
 
 app.listen(config.express.port, config.express.ip, function(error) {
   if(error) {
