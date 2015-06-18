8473e5783fee92c119e3f45d8bf2943f47ce4c3c
Alex Stoica <dimitrie.stoica@canopy-cloud.com>
2015-06-18 10:48:25 +0100

    took out rabbit mq configuration

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
 var app    = require('./index');
 var config = require('./config');
 var bole   = require('bole');
-var amqp   = require('amqp');
+var services = require('./services');
 
 bole.output({level: "debug", stream: process.stdout});
 var log = bole("server");
 log.info({env: process.env.NODE_ENV}, "Server process starting");
 
-console.log(config.rabbitmqURL)
-var rabbitmq = amqp.createConnection(config.rabitmqURL);
-rabbitmq.on('ready', function (callback) {
-  log.info("connected to: " + config.rabbitmqURL);
-});
+services.getRabbitMqConnection(function(connection) {
+  if (connection) {
 
-app.listen(config.express.port, config.express.ip, function(error) {
-  if(error) {
-    log.error("Unable to listen for connections", error);
-    process.exit(10);
-  }
-  log.info("Server is listening on http://" + 
-    config.express.ip + ":" + config.express.port);
+    log.info("Connected to RabbitMQ on: " + config.rabbitmqURL);
+
+    app.listen(config.express.port, config.express.ip, function(error) {
+      if(error) {
+        log.error("Unable to listen for connections", error);
+        process.exit(10);
+      }
+      log.info("Server is listening on http://" +
+      config.express.ip + ":" + config.express.port);
+    });
+  } else {
+      log.error("Unable to connect to RabbitMQ");
+    }
 });
 
 module.exports = app;
