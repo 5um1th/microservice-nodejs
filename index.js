57d8bd8ad3a6aba9cd3f52d1bcb1eefe18de7f86
Alex Stoica <dimitrie.stoica@canopy-cloud.com>
2015-07-14 19:02:49 +0100

    post request example to CF

--- index.js
+++ index.js
@@ -7,10 +7,15 @@ var info     = require('./lib/controllers/info');
 var sender   = require('./lib/services/sender');
 var receiver = require('./lib/services/receiver');
 var services = require('./services');
+var request = require('request');
 
 var mongodb = require( 'mongodb' ); 
 var MongoClient = mongodb.MongoClient;
 var assert = require('assert');
+
+app.get('/', function (req, res) {
+  res.send('Hello there !\n');
+});
 
 app.get('/send', function (req, res) {
   res.send('This page is sending data\n');
@@ -33,6 +38,26 @@ app.get('/info', function (req,res) {
     });
   });
   res.send(info.showInfo());
+});
+
+app.get('/data', function(req, res) {
+  var username= 'cf-requestor';
+  var password= 'GenEfbeeWrosFexofufupniwydHeur';
+  var url= 'https://cip-cf-api.apps.dev.labs.cf.canopy-cloud.com/cf';
+  var auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
+
+  // setting insecure conneciton
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+  request.post(
+    {
+      url : url,
+      headers : {
+        "Authorization" : auth
+      }
+    },function (error, response, body) {
+      res.send(body); 
+    });
 });
 
 app.use(function(req, res, next) {
